b11fa485e064716ea60f5a8303ddbf2e6d15a0e1
Sebastien Renault <sre@zenexity.com>
2014-06-20 15:45:56 +0200

    Expose ref id

--- src/main/java/io/prismic/Ref.java
+++ src/main/java/io/prismic/Ref.java
@@ -6,16 +6,22 @@ import com.fasterxml.jackson.databind.*;
 
 public class Ref {
 
+  final private String id;
   final private String ref;
   final private String label;
   final private boolean masterRef;
   final private DateTime scheduledAt;
 
-  public Ref(String ref, String label, boolean masterRef, DateTime scheduledAt) {
+  public Ref(String id, String ref, String label, boolean masterRef, DateTime scheduledAt) {
+    this.id = id;
     this.ref = ref;
     this.label = label;
     this.masterRef = masterRef;
     this.scheduledAt = scheduledAt;
+  }
+
+  public String getId() {
+    return id;
   }
 
   public String getRef() {
@@ -41,10 +47,11 @@ public class Ref {
   // --
 
   static Ref parse(JsonNode json) {
+    String id = json.path("id").asText();
     String ref = json.path("ref").asText();
     String label = json.path("label").asText();
     boolean masterRef = json.path("isMasterRef").asBoolean();
-    return new Ref(ref, label, masterRef, null);
+    return new Ref(id, ref, label, masterRef, null);
   }
 
 }
