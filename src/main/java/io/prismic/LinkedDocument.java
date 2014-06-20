922b30715e8329832ac1c85c67e936ab649c9d29
Sebastien Renault <sre@zenexity.com>
2014-06-20 17:27:02 +0200

    Expose slug in LinkedDocument

--- src/main/java/io/prismic/LinkedDocument.java
+++ src/main/java/io/prismic/LinkedDocument.java
@@ -6,16 +6,22 @@ import com.fasterxml.jackson.databind.JsonNode;
 public class LinkedDocument {
 
   private final String id;
+  private final String slug;
   private final String type;
   private final Set<String> tags;
 
-  public LinkedDocument(String id, String type, Set<String> tags) {
+  public LinkedDocument(String id, String slug, String type, Set<String> tags) {
     this.id = id;
+    this.slug = slug;
     this.type = type;
     this.tags = tags;
   }
 
   public String getId() {
+    return this.id;
+  }
+
+  public String getSlug() {
     return this.id;
   }
 
@@ -29,11 +35,12 @@ public class LinkedDocument {
 
   public static LinkedDocument parse(JsonNode json) {
     String id = json.path("id").asText();
+    String slug = json.has("slug") && !json.path("slug").isNull() ? json.path("slug").asText() : null;
     String type = json.path("type").asText();
     Set<String> tags = new HashSet<String>();
     for(JsonNode tagJson: json.withArray("tags")) {
       tags.add(tagJson.asText());
     }
-    return new LinkedDocument(id, type, tags);
+    return new LinkedDocument(id, slug, type, tags);
   }
 }
