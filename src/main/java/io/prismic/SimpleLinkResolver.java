82a99457c460e1ba2e66e80b353d341627c8de28
Erwan Loisant <eloisant@gmail.com>
2016-02-24 13:57:16 +0100

    whitespaces

--- src/main/java/io/prismic/SimpleLinkResolver.java
+++ src/main/java/io/prismic/SimpleLinkResolver.java
@@ -1,9 +1,9 @@
 package io.prismic;
 
 public abstract class SimpleLinkResolver implements LinkResolver {
-  
+
   public abstract String resolve(Fragment.DocumentLink link);
-  
+
   public String resolve(Document document) {
     return resolve(document.asDocumentLink());
   }
